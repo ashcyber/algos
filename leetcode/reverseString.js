b82a774dcfbfef7fa1a1d5600cbc0cc431a81900
Ash <ishans632@gmail.com>
2020-01-16 13:24:40 +0530

    reverseString

---
+++ leetcode/reverseString.js
@@ -0,0 +1,14 @@
+var reverseString = function(s) {
+    if(s.length === 0 || s === null) return []; 
+    
+    let i = 0; 
+    let j = s.length - 1; 
+
+    while(i <= j) { 
+        let temp = s[i]; 
+        s[i] = s[j]; 
+        s[j] = temp; 
+        i++; 
+        j--; 
+    }
+}
