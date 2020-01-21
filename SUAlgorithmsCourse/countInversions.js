ea89f64b83492466fc3ac4aa3714b253557d78ca
Ash <ishans632@gmail.com>
2020-01-22 00:18:33 +0530

    numberOfInversions

---
+++ SUAlgorithmsCourse/countInversions.js
@@ -0,0 +1,53 @@
+function countInversions(arr) { 
+    let inversions = 0; 
+    function mergeSort(arr) {
+        if(arr !== undefined) {
+            if(arr.length === 1) {  
+                return arr; 
+            }
+        
+            let mid = Math.floor(arr.length / 2) 
+            let left = mergeSort(arr.slice(0,mid)); 
+            let right = mergeSort(arr.slice(mid));
+        
+            return merge(left, right, count=0); 
+        }
+    }
+
+    function merge(left, right, count) { 
+        if(left === undefined && right === undefined) return []; 
+        if(right === undefined) return left; 
+        if(left === undefined) return right; 
+        
+        let i = 0; 
+        let j = 0;
+        let k = 0;  
+        let arr = new Array(left.length + right.length)
+    
+        while(i < left.length && j < right.length) { 
+            if(left[i] <= right[j]) { 
+                arr[k++] = left[i++]; 
+            }
+            else {
+                arr[k++] = right[j++];
+                inversions += left.length - i;  
+            }
+        }
+    
+        while(i < left.length) { 
+            arr[k++] = left[i++]; 
+        }
+    
+        while(j < right.length) {
+            arr[k++]  = right[j++]; 
+        }
+    
+        return arr; 
+    }
+
+    mergeSort(arr); 
+    return inversions; 
+}
+
+let ans = countInversions([3,2,1]); 
+console.log(ans); 
