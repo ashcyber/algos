0dee62bf707294dd0476641a9c6dded5ee8319c0
Ash <ishans632@gmail.com>
2020-02-25 21:10:23 +0530

    uniqOccurences

---
+++ leetcode/uniqOccurences.js
@@ -0,0 +1,24 @@
+var uniqueOccurrences = function(arr) {
+    let hm = {}; 
+    
+    for(let i = 0; i < arr.length; i++) { 
+        if(hm[arr[i]] === undefined) { 
+            hm[arr[i]] = 1; 
+        }else{
+            hm[arr[i]]++; 
+        }
+    }
+    
+    let keyHash = {}; 
+    let keyArr = Object.values(hm); 
+    for(let i = 0; i < keyArr.length; i++) { 
+        if(keyHash[keyArr[i]] === undefined) { 
+            keyHash[keyArr[i]] = 1; 
+        }
+        else { 
+            return false; 
+        }
+    }
+    
+    return true; 
+};
