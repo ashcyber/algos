f37c74b977aab2ccd4407d73eed5052c03075de8
Ash <ishans632@gmail.com>
2020-03-16 10:15:35 +0530

    minStackUpdate

--- leetcode/minStack.js
+++ leetcode/minStack.js
@@ -1,5 +1,15 @@
+class Node {
+    constructor(val, min) { 
+        this.val = val; 
+        this.min = min;
+        this.next = null;  
+
+    }
+}
+
+
 var MinStack = function() {
-    this.values = []; 
+    this.head = null; 
 };
 
 /** 
@@ -7,28 +17,36 @@ var MinStack = function() {
  * @return {void}
  */
 MinStack.prototype.push = function(x) { 
-    this.values.push(x); 
+    if(this.head === null) { 
+        this.head = new Node(x,x); 
+    }
+    else {
+        let newNode = new Node(x, Math.min(x, this.head.min)); 
+        newNode.next = this.head; 
+        this.head = newNode; 
+    }
 };
 
 /**
  * @return {void}
  */
 MinStack.prototype.pop = function() {
-    this.values.pop(); 
+    if(this.head === null) return null; 
+    let headVal = this.head.val; 
+    this.head = this.head.next; 
+    return headVal;  
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.top = function() {
-    return this.values[this.values.length - 1]; 
+    return this.head.val; 
 };
 
 /**
  * @return {number}
  */
 MinStack.prototype.getMin = function() {
-    return Math.min(...this.values); 
+    return this.head.min; 
 };
-
-
